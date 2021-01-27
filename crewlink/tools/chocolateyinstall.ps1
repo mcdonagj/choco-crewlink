1fb92ef7a0c5804bef0d49be89d9b7f08676e8c5
Gary McDonald <mcdonagj@dukes.jmu.edu>
2021-01-27 18:47:57 -0500

    Updated package to bundle CrewLink release 1.1.0.

--- crewlink/tools/chocolateyinstall.ps1
+++ crewlink/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ottomated/CrewLink/releases/download/v1.0.4/CrewLink-Setup-1.0.4.exe'
+$url        = 'https://github.com/ottomated/CrewLink/releases/download/v1.1.0/CrewLink-Setup-1.1.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,10 +8,10 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   url64bit      = $url64
-  softwareName  = 'CrewLink 1.0.4'
-  checksum      = '6A92B37C7F387A934A4B8DD98C2B5D6E'
+  softwareName  = 'CrewLink 1.1.0'
+  checksum      = '97DD8ED0BBD977866E97DDF4296EA966'
   checksumType  = 'md5'
-  checksum64    = 'A4CB1ED09AEFF97DEE1D56D92B889A1803F0C2CCC6337668EDE2B4455D64BE2C'
+  checksum64    = '5AE2700B04E8E0A1CD4CD448439520E21639CE0B2F6D594C525BB7E00F2CE4C3'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
